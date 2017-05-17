3a4bbb13ec8a173c6aa9bfbe33ae5a340cb86e30
dropcity <selmueller42@gmail.com>
2017-05-17 13:20:28 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ConwayGame
 Conway's Game of Life in R
+
+Project for "Funktionale und Objektorientierte Programmierung in R"
+- visualise Conway's Game of Life 
+- detect objects (still-life, oscillator, glider)
+
+Contributor:
+Anastasiia Aftakhova 
+Leila Feddoul
+Selina Müller
---
+++ conwaysGame.R
@@ -0,0 +1,62 @@
+getNeighbours <- function(M, i, j) {
+  # Selina
+  # TODO: finde Nachbarn von M(i,j)
+  # Beachte den Randfall: bsp. für linke obere Ecke:
+  # Nachbar oben: von unten
+  # Nachbar links: von rechts
+  # Nachbar oben links: von rechts unten
+  
+  # return 3x3 Matrix (M(i,j) in der Mitte und seine Nachbarn)
+}
+
+computeIsAlive <- function(N) {
+  # Leila
+  # TODO: 
+  # gegeben 3x3 Nachbarmatrix für ein Wert
+  # Regeln des Spiels implementieren
+  # return 0(Tot) oder 1(Lebend)
+}
+
+
+computeAll <- function(M, save_path) {
+  # Selina
+  # TODO: iteriere über alle MAtrix elemente
+  # berechne die Nachbar-Matrix N = getNeighbours(M, i, j)
+  # rufe computeIsAlive(N) auf
+}
+
+visualise <- function(M) {
+  # Ana
+  # TODO: die Matrix als Plot in R anzeigen
+  # achte auf korrekte Orientierung
+  # bzw. Farbwerte
+}
+
+
+save <- function(M, save_path, iter_id) {
+  # Ana
+  # TODO:
+  # speichere aktuelle Matrix als .png? unter save_path
+  # in dem Namen Iterationszahl
+}
+
+createMatrix <- function(matrix_size) {
+  # Leila
+  # TODO:
+  # eine quadr. Matrix von gegebener Größe erstellen
+  # Matrix mit zufälligen Werten (0 oder 1) füllen
+  # return M (matrix_size x matrix_size)
+}
+
+
+starteSpiel <- function(iter_number, matrix_size, save_path) {
+  M <- createMatrix(matrix_size)
+  for (i in c(1:iter_number)) { # check if for-Loop correct
+    computeAll(M)
+    visualise(M)
+    save(M, save_path, i)
+  }
+}
+
+# Spiel starten: 
+# starteSpiel(1000, 300, '..blabla');
