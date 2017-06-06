8b6484f1d8d446ec20ade4cba0bc51396b55528c
leilafedd <leilafedd@gmail.com>
2017-06-06 23:41:55 +0200

    Create findPatternMatch_2 .R

---
+++ findPatternMatch_2 .R
@@ -0,0 +1,25 @@
+
+#prüft ob zwei Matrizen gleich sind
+matequal <- function(x, y)
+  is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
+  
+#findPatternMatch_2 from Leila
+findPatternMatch_2<-function(mask,M,col)
+{
+  maskSize<-dim(mask)
+  nrM<-nrow(M)
+  ncM<-ncol(M)
+  for(i in 1:(nrM-maskSize[1]))
+  {
+    for(j in 1:(ncM-maskSize[1]))
+    {
+    N<-M[i:(i+(maskSize[1]-1)),j:(j+(maskSize[1]-1))]
+    if(matequal(mask,N))
+    {
+      N[which(N == 1)] <- col
+      M[i:(i+(maskSize[1]-1)),j:(j+(maskSize[1]-1))]<-N
+    }
+    }
+  }
+  return(M)
+  }
